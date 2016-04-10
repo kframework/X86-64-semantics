4ffcc65da6e3110cb60d6ce56310d34bf4185b52
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-04-09 23:35:14 -0500

    init

--- utils/install_llvm.sh
+++ utils/install_llvm.sh
@@ -6,7 +6,7 @@ if [ "test$LLVM_HOME" == "test" ]; then
 	echo "Please set the LLVM_HOME env!"
 	exit 1
 fi
-
+set -x
 VERSION=3.5
 [ $# -ge 1 ] && VERSION=$1
 case $VERSION in
@@ -16,9 +16,9 @@ case $VERSION in
                 wget http://llvm.org/releases/3.5.0/cfe-3.5.0.src.tar.xz
                 wget http://llvm.org/releases/3.5.0/llvm-3.5.0.src.tar.xz
                 wget http://llvm.org/releases/3.5.0/compiler-rt-3.5.0.src.tar.xz
-                tar xf llvm-3.5.0.src.tar.xz && rm -f llvm-3.5.0.src.tar.xz
-                tar xf cfe-3.5.0.src.tar.xz && rm -f cfe-3.5.0.src.tar.xz
-                tar xf compiler-rt-3.5.0.src.tar.xz && rm -f compiler-rt-3.5.0.src.tar.xz
+                tar -xvf llvm-3.5.0.src.tar.xz && rm -f llvm-3.5.0.src.tar.xz
+                tar -xvf cfe-3.5.0.src.tar.xz && rm -f cfe-3.5.0.src.tar.xz
+                tar -xvf compiler-rt-3.5.0.src.tar.xz && rm -f compiler-rt-3.5.0.src.tar.xz
                 mv llvm-3.5.0.src $LLVM_HOME
                 mv cfe-3.5.0.src $LLVM_HOME/tools/clang
                 mv compiler-rt-3.5.0.src $LLVM_HOME/projects/compiler-rt
@@ -36,3 +36,5 @@ cd $LLVM_HOME/build
 			 --enable-shared
 make -j2
 make install
+
+set +x
