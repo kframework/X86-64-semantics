cb76e43af45525c6f8c5e39d2f303ac28ae9d385
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-04-11 01:43:40 -0500

    max stack height

--- source/lib/max_stack_height/max-stack-height.cpp
+++ source/lib/max_stack_height/max-stack-height.cpp
@@ -1,4 +1,10 @@
+
+#define DEBUG_TYPE "max_stack_height"
 #include "max_stack_height.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Debug.h"
 
 
 using namespace llvm;
@@ -16,16 +22,38 @@ max_stack_height::runOnFunction(Function &F) {
   return false; // Analysis pass
 }
 
+/*******************************************************************
+  * Function :   perform_dfa
+  * Purpose  :   Entry point for DFA
+********************************************************************/
 void
 max_stack_height::perform_dfa() {
+  initialize_dfa_framework();
+  
+  perform_const_dfa();
+  
+  perform_global_dfa();
+
+  print_flow_equations();
 
 }
 
+/*******************************************************************
+  * Function :   initialize_dfa_framework
+  * Purpose  :   Allocates the data flow values
+********************************************************************/
 void
 max_stack_height::initialize_dfa_framework() {
-
+  for (Function::iterator BB = Func->begin(), E = Func->end(); BB != E; ++BB) {
+    dfva* dfvaInstance = new dfva(3,0);
+    BBMap[BB] = dfvaInstance;
+  }
 }
 
+/*******************************************************************
+ * Function :   perform_const_dfa
+ * Purpose  :   Calculate the local sets GEN
+********************************************************************/
 void
 max_stack_height::perform_const_dfa() {
 
@@ -36,7 +64,29 @@ max_stack_height::perform_global_dfa() {
 
 }
 
+/*******************************************************************
+ * Function :   print_flow_equations
+ * Purpose  :   print the data flow values for each BB
+********************************************************************/
 void
 max_stack_height::print_flow_equations() {
 
+  dfva* dfvaInstance;
+  StringRef Fname = Func->getName();
+  for (Function::iterator BB = Func->begin(), E = Func->end(); BB != E; ++BB) {
+    dfvaInstance = BBMap[BB];
+    StringRef BBname = (*BB).getName();
+    DEBUG(errs() << Fname << "::" << BBname << "\n");
+    for(uint32_t i = 0; i < TOTAL_DFA_VALUES; i++){
+  
+      switch(i) {
+        case(IN) : DEBUG(errs() << "    IN "); break;
+        case(GEN) : DEBUG(errs() << "  GEN "); break;
+        case(OUT) : DEBUG(errs() << "  OUT "); break;
+      }
+      
+      DEBUG(errs() << (*dfvaInstance)[i]); 
+    }
+    DEBUG(errs() << "\n------------\n"); 
+  }
 }
