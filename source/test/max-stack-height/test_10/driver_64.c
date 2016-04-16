c52912bbc5a3c41fbe17a110d456a2573c244b08
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-04-16 18:33:16 -0500

    testing

---
+++ source/test/max-stack-height/test_10/driver_64.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "mc-sema/common/RegisterState.h"
+
+extern void mcsema_main(RegState *);
+
+int doDemo2(int k) {
+    RegState            rState = {0};
+    unsigned long   stack[4096*10];
+
+    //set up the stack 
+    rState.RSP = (uint64_t) &stack[4096*9];
+    rState.RAX = k;
+
+    mcsema_main(&rState);
+
+    return rState.RAX;
+}
+
+int main(int argc, char *argv[]) {
+  size_t  len = sizeof("/first/test/path");
+  char    *a = malloc(len);
+  char    *b = malloc(len);
+
+  memset(b, 0, len);
+  strcpy(a, "/first/test/path");
+
+  RegState            rState = {0};
+    unsigned long   stack[4096*10];
+
+    //set up the stack 
+    rState.RSP = (uint64_t) &stack[4096*9];
+    rState.RDI = (uint64_t) a;
+    rState.RSI = (uint64_t) b;
+
+    mcsema_main(&rState);
+
+    printf("%s -> %s\n", a, b);
+
+    free(a);
+    free(b);
+
+    return rState.RAX;
+}
