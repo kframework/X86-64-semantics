3d03364e1fa8f9c8e4b83989d38ea2ea7052841e
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-04-15 22:54:12 -0500

    testing

---
+++ source/test/max-stack-height/test_4/test_4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int foo(int a, int b) { 
+  int temp1, temp2; 
+  temp1 = a+b;
+
+  while(a >= 0) {
+    if(a%2 == 0 ) {
+      temp2 = temp1 + 10;
+    } else {
+      temp1 = temp1 - 10;
+    }
+    a --;
+  }
+  return temp2;
+}
+
+int main() {
+  int z; 
+  z = foo(10,20); 
+  return z;
+}
+
