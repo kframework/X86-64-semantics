52764eff338997983150316cd0a2d604813ca67b
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-04-16 20:22:13 -0500

    testing

---
+++ source/test/max-stack-height/test_18/driver_64.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "mc-sema/common/RegisterState.h"
+
+extern void mcsema_main(RegState *);
+
+int main(int argc, char *argv[]) {
+
+  RegState            rState = {0};
+  unsigned long   stack[4096*10];
+
+  //set up the stack 
+  rState.RSP = (uint64_t) &stack[4096*9];
+
+  return rState.RAX;
+}
