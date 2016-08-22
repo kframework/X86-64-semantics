8cffa2b17540da97b2196d24a0c13fce2dd30242
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-08-22 17:22:19 -0500

    stack-decons

--- source/stack_deconstructor/stack_deconstructor.cpp
+++ source/stack_deconstructor/stack_deconstructor.cpp
@@ -38,18 +38,57 @@ bool stack_deconstructor::runOnModule(Module &M) {
   return true; // Transform pass
 }
 
+/*******************************************************************
+  * Function :   insertlocalstack
+  * Purpose  :   Deconstruct the global stack frame (visible to all the 
+  *               functions) into per procedure (local) stack frame.
+  *              Covert the following instructiions 
+  *
+  *               %RSP_val = alloca i64, !mcsema_real_eip !2 
+  *               
+  *               %RSP = getelementptr inbounds %struct.regs* %0, i64 0, i32 6, !mcsema_real_eip !2 ; Reading the register context to get the stack pointer
+  *               %7 = load i64* %RSP, !mcsema_real_eip !2
+  *
+  *               store i64 %7, i64* %RSP_val, !mcsema_real_eip !2  ; Storing the stack pointer in the local variable %RSP_val
+  *               ; All subsequesnt computations are using %RSP_val
+  *
+  *               TO
+  *
+  *               %RSP_val = alloca i64, !mcsema_real_eip !2
+  *               
+  *               ; Following two are dead instructions
+  *               %RSP = getelementptr inbounds %struct.regs* %0, i64 0, i32 6, !mcsema_real_eip !2 ; 
+  *               %7 = load i64* %RSP, !mcsema_real_eip !2
+  *
+  *               %_local_stack_alloc_ = alloca [32 x i64]   ; The stack height 32 is determined by a previous dfa pass    ; Newly inserted
+  *               %_local_stack_gep_ = getelementptr inbounds [32 x i64]* %_local_stack_alloc_, i32 0, i32 0  ; Newly inserted
+  *               %_local_stack_P2I_ = ptrtoint i64* %_local_stack_gep_ to i64                                ; Newly inserted
+  *               store i64 %_local_stack_P2I_, i64* %RSP_val                                                 ; Newly inserted
+  *
+  *               ; All subsequesnt computations are using %RSP_val
+********************************************************************/
 void stack_deconstructor::insertlocalstack(Function &F, height_ty size) {
 
-  BasicBlock &eb = F.getEntryBlock();
   assert(size <= 0 && "stack height cannot be positive\n");
+
+  BasicBlock &eb = F.getEntryBlock();
   PtrToIntInst *p2i_inst = NULL;
   StoreInst *str_inst = NULL;
   LLVMContext &ctx =  Mod->getContext();
 
-  //for(auto &I : eb) {
+  bool stack_frame_deconstructed = false;
+
+  /*  Search for the instruction 
+   *    store i64 %7, i64* %RSP_val
+   *  in the entry block
+   */  
   for (BasicBlock::iterator i = eb.begin(); i != eb.end(); ) {  
     Instruction *I = i++;
-    if(NULL == p2i_inst && NULL != (str_inst = dyn_cast<StoreInst>(I))) {
+    //if(NULL == p2i_inst && NULL != (str_inst = dyn_cast<StoreInst>(I))) {
+    if(NULL != (str_inst = dyn_cast<StoreInst>(I))) {
+
+      stack_frame_deconstructed = true;
+
       DEBUG(errs() << *str_inst << "\n");
       Value* ptr_operand = str_inst->getPointerOperand();
       if(ptr_operand->getName().equals("RSP_val")) {
@@ -71,32 +110,38 @@ void stack_deconstructor::insertlocalstack(Function &F, height_ty size) {
 
         //create:: store i64 %_local_stack_P2I_, i64* %RSP_val
         new StoreInst(p2i_inst, ptr_operand, str_inst);
+
         str_inst->eraseFromParent();
       }
     }
+  }
 
-    /* For each call inst (to mcsema generated functions), add an extra actual argument %_local_stack_P2I_
-    ** and also the corresponding called function are augmented with an extra argument i64 %_parent_rsp_   
-    */
-    if(NULL != p2i_inst) {
-      if(CallInst *ci = dyn_cast<CallInst>(I)) {
-        Function* f = ci->getCalledFunction();
-        if(!f->isDeclaration()) {
+  //If the stack frame is not deconstructed, return
+  if(false == stack_frame_deconstructed) {
+    return;
+  }
 
-          Function* new_f = cloneFunctionWithExtraArgument(f);
+  return;
 
-          std::vector<Value*> arguments;
-          for(auto &args : ci->arg_operands()) {
-            arguments.push_back(args);
-          }
+  /* For each call inst (to mcsema generated functions), add an extra actual argument %_local_stack_P2I_
+  ** and also the corresponding called function are augmented with an extra argument i64 %_parent_rsp_   
+  if(CallInst *ci = dyn_cast<CallInst>(I)) {
+    Function* f = ci->getCalledFunction();
+    if(!f->isDeclaration()) {
 
-          arguments.push_back(p2i_inst);
-          CallInst::Create(new_f, arguments, "", ci);
-          ci->eraseFromParent();
-        }
+      Function* new_f = cloneFunctionWithExtraArgument(f);
+
+      std::vector<Value*> arguments;
+      for(auto &args : ci->arg_operands()) {
+        arguments.push_back(args);
       }
+
+      arguments.push_back(p2i_inst);
+      CallInst::Create(new_f, arguments, "", ci);
+      ci->eraseFromParent();
     }
   }
+  */
 }
 
 Function* stack_deconstructor::cloneFunctionWithExtraArgument(Function * F) {
