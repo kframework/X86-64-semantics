47afd97646328e24e1129d63763f3280e8ec13b2
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-10-02 23:38:21 -0500

    tool name changed

--- CheckFormat.cmake
+++ CheckFormat.cmake
@@ -14,26 +14,26 @@ set(CLANGFORMAT_COMMAND clang-format -sort-includes -style=llvm)
 # cmake for loop, let's do this
 set(i 0)
 foreach(file IN LISTS files)
-  add_custom_command(OUTPUT allina-check-format${i}
+  add_custom_command(OUTPUT allin-check-format${i}
     COMMAND ${CLANGFORMAT_COMMAND} ${file} | diff -u ${file} -
     VERBATIM
     COMMENT "Checking format of ${file}..."
   )
-  list(APPEND check_format_depends "allina-check-format${i}")
+  list(APPEND check_format_depends "allin-check-format${i}")
 
-  add_custom_command(OUTPUT allina-update-format${i}
+  add_custom_command(OUTPUT allin-update-format${i}
     COMMAND ${CLANGFORMAT_COMMAND} -i ${file}
     VERBATIM
     COMMENT "Updating format of ${file}..."
   )
-  list(APPEND update_format_depends "allina-update-format${i}")
+  list(APPEND update_format_depends "allin-update-format${i}")
 
   math(EXPR i ${i}+1)
 endforeach()
 
 add_custom_target(check-format DEPENDS ${check_format_depends})
-set_target_properties(check-format PROPERTIES FOLDER "ALLINA")
+set_target_properties(check-format PROPERTIES FOLDER "ALLIN")
 
 add_custom_target(update-format DEPENDS ${update_format_depends})
-set_target_properties(update-format PROPERTIES FOLDER "ALLINA")
+set_target_properties(update-format PROPERTIES FOLDER "ALLIN")
 
