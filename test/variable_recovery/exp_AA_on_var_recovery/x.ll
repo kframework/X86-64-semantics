766c17393282af2c4ac6cd8b7bd64ce93afe5ae0
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-10-18 17:34:18 -0500

    testsuite restructure

--- test/variable_recovery/exp_AA_on_var_recovery/x.ll
+++
@@ -1,7 +0,0 @@
-define i32 @main() {
-entry:
-	%0 = alloca i8
-	%1 = alloca i8
-	sub i8* %0, %1
-	 ret i32 0
-}
--- test/variable_recovery/utils/clearall.sh
+++
@@ -1,3 +0,0 @@
-rm -f *.cfg  *.o *.exe  *.bc *.lifted  *.log *_opt.ll  mapping.txt out 
-ls *.ll | grep -v clang | grep -v gcc | xargs rm -rf 
-ls *.objdump | grep -v clang | grep -v gcc | xargs rm -rf 
