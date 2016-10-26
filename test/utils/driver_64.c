6190d4558261c9577c6f841b6f4bd17623de6ec0
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-10-26 03:56:22 -0500

    testing mcsema

---
+++ test/utils/driver_64.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "RegisterState.h"
+
+extern void mcsema_main(RegState *);
+
+int main(int argc, char *argv[]) {
+
+  RegState            rState = {0};
+  unsigned long   stack[4096*10];
+
+  //set up the stack 
+  rState.RSP = (uint64_t) &stack[4096*9];
+  mcsema_main(&rState);
+
+  return 0;
+}
