72f3ee05145a4d554c37ffdc97d1fe372f2d62fe
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-10-27 13:36:20 -0500

    evaluating mcsema

--- test/utils/test_mcsema.pl
+++ test/utils/test_mcsema.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use File::Compare;
+  use File::Basename;
 
 #Global constants
 my $home =  $ENV{'HOME'};
@@ -29,16 +30,21 @@ my $CFGBC_ARCH="";
 # Customizable inputs
 my $help = "";
 my $entry="main";
+my $check_unsupported="";
 my $incdir="${home}/Github/binary-decompilation/test/utils/";
 
 # Status
 my $diffcount = 0;
-my $cfgerror = 0;
+my $cfg_to_bc_error = 0;
 my $total = 0;
+my $unsupported = 0;
+my $supported = 0;
+my @nf = ();
 
 GetOptions (
             "help"          => \$help, 
             "entry:s"       => \$entry, 
+            "check_unsupported"       => \$check_unsupported, 
             ) or die("Error in command line arguments\n");
 
 
@@ -58,59 +64,109 @@ open(my $fp, "<", "filelist.txt") or die "cannot open: $!";
 my @files = <$fp>;
 for my $file (@files) {
   chomp $file;
-  my $orig_exec = $file;
-  my $trans_exec = $file.".lifted";
+  my $lifted = $file.".lifted";
+  my $cfg = $file . ".cfg";
+
+  my $bc = $file . ".new.bc";
+  my $optbc = $file . ".new.opt.bc";
+  my $o = $file . ".new.o";
+  my $newlifted = $file . ".new.lifted";
+  my $cfg2bclog = $file . ".new.convert.txt";
 
   $total =  $total  + 1;
 
-  print "=============================\n";
-  print $orig_exec . "\n";
+  #print "\n=============================\n";
+  #print $file . "\n";
 
-  if(!(-e $trans_exec)) {
-    print $trans_exec. "?\n";
-    my $cfg = $file . ".cfg";
-    my $bc = $file . ".bc";
-    my $optbc = $file . ".opt.bc";
-    my $o = $file . ".o";
-    my $lifted = $file . ".lifted";
-    my $else = $file . ".*";
-
-    if(!(-e $cfg)) {
-      print $cfg. "?\n";
-    } else {
-      execute("${CFG_TO_BC_PATH}/cfg_to_bc -ignore-unsupported ${CFGBC_ARCH}  -i $cfg  -o $bc  -driver=mcsema_main,${entry},raw,return,C > /dev/null");
-      execute("${OPT} -O3    $bc  -o=$optbc"); 
-      execute("${LLC} ${BIN_ARCH} -filetype=obj -o $o $optbc");
-      execute("${CC}  -g ${GCC_ARCH} -I${incdir} -o $lifted $driver $o ${libnone}");
-      execute("ls $else");
-      if(-e $lifted) {
-
-      } else {
-        $cfgerror = $cfgerror +1;
-      
-      }
-    }
+  if(-e $lifted) {
+    $supported += 1;
+    process_lifted();
   } else {
-    execute("$orig_exec 2>&1  1>out1");
-    execute("$trans_exec 2>&1  1>out2");
-    if (compare("out1","out2") == 0) {
-      #print "They're equal\n";
-    } else {
-      print "Diff Output\n";
-      $diffcount  = $diffcount + 1;
-    }
-    execute("rm -rf  out1 out 2");
+    $unsupported += 1;
+    push @nf, $file;
+    process_cfg($file);
   }
-
 }
 
 print "Total: " . $total . "\n";
 print "Diff Error " . $diffcount . "\n";
-print "CFG  Error " . $cfgerror . "\n";
+print "CFG  Error " . $cfg_to_bc_error . "\n";
+print "Unsupported  " . $unsupported . "\n";
+print "Supported  " . $supported . "\n";
+
+for my $file (@nf) {
+  print $file . "\n";
+}
+
+
 
 # Utilities
+sub process_lifted {
+
+}
+
+sub process_cfg {
+  my $file = shift @_;
+
+  my $lifted = $file.".lifted";
+  my $cfg = $file . ".cfg";
+  my @suffixlist = (".simple");
+
+  my $bc = $file . ".new.bc";
+  my $optbc = $file . ".new.opt.bc";
+  my $o = $file . ".new.o";
+  my $newlifted = $file . ".new.lifted";
+  my $cfg2bclog = $file . ".new.convert.txt";
+  my ($basename,$path,$suffix) = fileparse($file,@suffixlist);
+
+  if($check_unsupported ne "") {
+    #execute("grep \"Unsupported\" -A 2 $cfg2bclog");
+    execute("grep \"Unsupported\" -A 2 $path/convert.log");
+  } else {
+    #-ignore-unsupported 
+    execute("${CFG_TO_BC_PATH}/cfg_to_bc  ${CFGBC_ARCH}  -i $cfg  -o $bc  -driver=mcsema_main,${entry},raw,return,C &> $cfg2bclog");
+    execute("${OPT} -O3    $bc  -o=$optbc"); 
+    execute("${LLC} ${BIN_ARCH} -filetype=obj -o $o $optbc");
+    execute("${CC}  -g ${GCC_ARCH} -I${incdir} -o $newlifted $driver $o ${libnone}");
+    if(-e $newlifted) {
+      run_compare($file, $newlifted);
+    } else {
+      $cfg_to_bc_error = $cfg_to_bc_error +1;
+    }
+  }
+}
+
+sub run_compare {
+  my $orig = shift @_;
+  my $lifted = shift @_;
+  execute("$orig 2>&1  1>out1");
+  execute("$lifted 2>&1  1>out2");
+  if (compare("out1","out2") == 0) {
+  } else {
+    execute("diff out1 out2");
+    $diffcount  = $diffcount + 1;
+  }
+  execute("rm -rf  out1 out 2");
+}
+
 sub execute {
   my $args = shift @_;
   print "$args \n";
   system("$args");
 }
+
+sub split_filename {
+    my $arg = shift @_;
+
+    if("" eq $arg) {
+      return ("", "");
+    }
+    my @components = split (/\//, ${arg}); 
+    my $filename = $components[@components -1];
+    @components = split (/\./, ${filename}); 
+    my $file = $components[0];
+    my $ext = $components[1];
+    return ($file,$ext);
+}
+
+
