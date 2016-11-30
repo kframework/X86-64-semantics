65dd3ad1c0080fa8a5266c23e84f6dc02de23179
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-11-30 11:50:48 -0600

    cleaning up master branch

--- test/decompilation/test_29/test_29.c
+++ test/decompilation/test_29/test_29.c
@@ -1,14 +1,14 @@
 //extern int printf(const char*, ...);
 
 //void foo( unsigned int i, int array[4]) __attribute__((noinline));
-
+#include<stdio.h>
 void foo( unsigned int i, int array[4]) {
   unsigned int j;
   for (j=3; j>i; j--)
     array[j] = array[j-1];
   printf("%d %d %d %d %d\n", i, array[0], array[1], array[2], array[3]);
 }
-main() {
+int main() {
   int array[4], i;
   for (i=0; i<5; i++) {
     array[0] = 5; array[1] = 6; array[2] = 7; array[3] = 8;
