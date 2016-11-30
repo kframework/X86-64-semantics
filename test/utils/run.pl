8257c1c0077f0eff29e32712100b60458cdc83c1
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-11-30 02:38:35 -0600

    migration to new reg assign

--- test/utils/run.pl
+++ test/utils/run.pl
@@ -2,8 +2,8 @@
 use strict;
 use warnings;
 use Getopt::Long;
-
-
+use File::Compare;
+use File::Basename;
 
 #Global constants
 my $home =  $ENV{'HOME'};
@@ -18,6 +18,8 @@ my $LLVMAS35="${home}/Install/llvm-3.5.0.release.install/bin/llvm-as";
 my $LLC="llc";
 my $outdir="Output/";
 my $CC_OPTIONS="";
+my $CC_35="${home}/Install/llvm-3.5.0.release.install/bin/clang-3.5";
+my $libnone=$ENV{'LIBNONE'};
 #"-fomit-frame-pointer";
 #my $redirect = " &> ";
 
@@ -33,16 +35,23 @@ my $print="";
 my $map="";
 my $entry="";
 my $incdir="";
-my $libnone="";
 my $cfg="";
 my $bind="";
+my $extract_bc="";
+my $reg_assign="";
+my $runpass="";
+my $stdin_args="";
+my $cmd_args="";
 
 GetOptions (
             "help"          => \$help, 
             "print"         => \$print, 
             "skip_mcsema"   => \$skip_mcsema, 
             "cfg"           => \$cfg, 
+            "extract_bc"    => \$extract_bc, 
+            "reg_assign"    => \$reg_assign, 
             "bind"           => \$bind, 
+            "runpass"      => \$runpass, 
             "compiler:s"    => \$compiler, 
             "home:s"       => \$MCSEMA_HOME, 
             "arch:s"        => \$arch, 
@@ -51,11 +60,12 @@ GetOptions (
             "suffix:s"      => \$suffix, 
             "entry:s"       => \$entry, 
             "incdir:s"      => \$incdir, 
-            "libnone:s"     => \$libnone, 
+            "stdin_args:s"      => \$stdin_args, 
+            "cmd_args:s"      => \$cmd_args, 
             ) or die("Error in command line arguments\n");
 
 if($help) {
-  print ("run.pl   \n");
+  print ("~/Github/binary-decompilation/test/utils/run.pl -home ~/Github/mcsema_new_reg_assign/build/ -file test_28.c -extract_bc -reg_assign -entry main -print -incdir ~/Github/binary-decompilation/test/utils/ \n");
   exit(1);
 }
 
@@ -69,6 +79,12 @@ my $CFG_TO_BC_PATH="${MCSEMA_HOME}/mc-sema/bitcode_from_cfg/";
 my $GCC_ARCH="";
 my $BIN_ARCH="";
 my $CFGBC_ARCH="";
+my $loadso="${ALLIN_HOME}/build/lib/LLVMstack_deconstructor.so";
+my $OPTSWITCH="-early-cse-memssa -ssh" ;
+#my $OPTSWITCH="-stack-decons -mem2reg -dce  -early-cse-memssa";
+#my $OPTSWITCH="-stack-decons -debug-only=\"stack_deconstructor\"";
+
+
 
 
 if($arch eq "32") {
@@ -88,18 +104,33 @@ if("" ne $bind) {
 
 my ($basename, $ext) = split_filename($file);
 
+### Drivers
+if("" ne $extract_bc) {
+  extract_bc_from_cfg();
+  generate_linked_binary("${outdir}${basename}.${suffix}.bc", "${outdir}${basename}.${suffix}.lifted.exe");
+  cleanup();
+  exit;
+}
+
+if("" ne $runpass) {
+  run_custom_pass();
+  exit;
+}
+
 if ("" ne $cfg) {
   generate_binary_from_source();
   generate_cfg();
 } else {
-  run_mcsema();
-  generate_linked_binary();
+  extract_bc_from_cfg();
+  generate_linked_binary("${outdir}${basename}.${suffix}.bc", "${outdir}${basename}.${suffix}.lifted.exe");
   cleanup();
 }
 
+
 # Functions
 sub generate_binary_from_source {
-  print("Generate source binary\n");
+  print("\nGenerate source binary\n");
+  execute("rm -rf ${outdir}${basename}.${suffix}.o ${outdir}${basename}.${suffix}.objdump");
   if("asm" eq $ext) {
     execute("nasm -f elf64 -o ${outdir}${basename}.${suffix}.o $file ;");
   } 
@@ -117,15 +148,28 @@ sub generate_binary_from_source {
 
 
 sub generate_linked_binary {
-  print("Generate lifted binary\n");
+  print("\nGenerate lifted binary\n");
+  my $inputbc = shift @_;
+  my $outputexe = shift @_;
+
+  execute("rm -rf ${outputexe}");
+
   #execute("${LLC} ${BIN_ARCH} -filetype=obj -o ${outdir}${basename}.${suffix}.lifted.o ${outdir}${basename}.${suffix}.opt.ll");
   #execute("${CC}  -g ${GCC_ARCH} -I${incdir} -o ${outdir}${basename}.${suffix}.lifted.exe driver_64.c ${outdir}${basename}.${suffix}.lifted.o ${libnone}");
-  execute("${CC} ${GCC_ARCH}  -o ${outdir}${basename}.${suffix}.lifted.o -c ${outdir}${basename}.${suffix}.opt.ll");
-  execute("${CC}  -g ${GCC_ARCH} -I${incdir} -o ${outdir}${basename}.${suffix}.lifted.exe driver_64.c ${outdir}${basename}.${suffix}.lifted.o ${libnone}");
+  if("" ne $reg_assign) {
+    #execute("${CC_35} -O3 ${GCC_ARCH} $inputbc $MCSEMA_HOME/../drivers/ELF_64_linux.S ${libnone}  -o $outputexe");
+    execute("${CC} -O3 ${GCC_ARCH} $inputbc $MCSEMA_HOME/../drivers/ELF_64_linux.S ${libnone}  -o $outputexe");
+  } else {
+    execute("${CC} ${GCC_ARCH}  -o ${outdir}${basename}.${suffix}.lifted.o -c ${outdir}${basename}.${suffix}.opt.ll");
+    execute("${CC}  -g ${GCC_ARCH} -I${incdir} -o ${outdir}${basename}.${suffix}.lifted.exe driver_64.c ${outdir}${basename}.${suffix}.lifted.o ${libnone}");
+  }
 }
 
 sub generate_cfg {
   print("\nGenerate cfg ($cfgext)\n");
+
+  execute("rm -rf ${outdir}${basename}.${suffix}${cfgext}.cfg ${outdir}${basename}.${suffix}.ida.log");
+
   #execute("IDA_PATH=${home}/ida-6.95 ${BIN_DESCEND_PATH}/bin_descend_wrapper.py -d ${BIN_ARCH} -func-map=${map} -entry-symbol=${entry} -i=${outdir}${basename}.${suffix}.o"); 
   if("" ne $bind) {
     execute("${BIN_DESCEND_PATH}/bin_descend  ${BIN_ARCH} -d -i=${outdir}${basename}.${suffix}.o -func-map=${map}  -entry-symbol=${entry} &> /tmp/bd.log ");
@@ -134,14 +178,22 @@ sub generate_cfg {
   }
 }
 
-sub run_mcsema {
-  print("Running cfg to bc\n");
+###  Generate BC from CFG
+sub extract_bc_from_cfg {
+  print("\nRunning cfg to bc\n");
   if("" ne $skip_mcsema) {
     return;
   }
+
   if(-e "${outdir}${basename}.${suffix}${cfgext}.cfg") {
     execute("rm -rf ${outdir}${basename}.${suffix}.bc ${outdir}${basename}.${suffix}.opt.bc ${outdir}${basename}.${suffix}.ll ${outdir}${basename}.${suffix}.opt.ll");
-    execute("${CFG_TO_BC_PATH}/cfg_to_bc -ignore-unsupported ${CFGBC_ARCH}  -i ${outdir}${basename}.${suffix}${cfgext}.cfg  -o ${outdir}${basename}.${suffix}.bc  -driver=mcsema_main,${entry},raw,return,C 1> ${outdir}cfgbc.log 2>&1");
+
+    if("" ne "$reg_assign") {
+      execute("${CFG_TO_BC_PATH}/cfg_to_bc -post-analysis=0 ${CFGBC_ARCH}  -i ${outdir}${basename}.${suffix}${cfgext}.cfg  -o ${outdir}${basename}.${suffix}.bc  -entrypoint=${entry} 1> ${outdir}${basename}.${suffix}.cfg2bc.log 2>&1");
+    } else {
+      execute("${CFG_TO_BC_PATH}/cfg_to_bc -ignore-unsupported ${CFGBC_ARCH}  -i ${outdir}${basename}.${suffix}${cfgext}.cfg  -o ${outdir}${basename}.${suffix}.bc  -driver=mcsema_main,${entry},raw,return,C 1> ${outdir}${basename}.${suffix}.cfg2bc.log 2>&1");
+    }
+
     execute("${OPT} -O3    ${outdir}${basename}.${suffix}.bc  -o=${outdir}${basename}.${suffix}.opt.bc"); 
     execute("${LLVMDIS}   ${outdir}${basename}.${suffix}.bc -o=${outdir}${basename}.${suffix}.ll");
     execute("${LLVMDIS}   ${outdir}${basename}.${suffix}.opt.bc -o=${outdir}${basename}.${suffix}.opt.ll");
@@ -150,6 +202,59 @@ sub run_mcsema {
     exit(1);
   }
 }
+
+### Run my passes
+sub run_custom_pass {
+  print("\nRunning custom passes\n");
+
+  execute("rm -rf ${outdir}${basename}.${suffix}.trans.bc ${outdir}${basename}.${suffix}.trans.opt.bc ${outdir}${basename}.${suffix}.trans.ll ${outdir}${basename}.${suffix}.trans.opt.ll");
+
+  execute("${OPT} -load=${loadso} ${OPTSWITCH} ${outdir}${basename}.${suffix}.ll  -o ${outdir}${basename}.${suffix}.trans.bc  2>  ${outdir}${basename}.${suffix}.pass.log"); 
+  #${ALLIN} ${outdir}${basename}.${ext}.ll -o ${outdir}${basename}.${ext}.trans.bc 2>  ${outdir}${basename}.${ext}.pass.log
+  execute("${OPT} -O3  ${outdir}${basename}.${suffix}.trans.bc -o=${outdir}${basename}.${suffix}.trans.opt.bc");
+  execute("${LLVMDIS} ${outdir}${basename}.${suffix}.trans.bc -o ${outdir}${basename}.${suffix}.trans.ll");
+  execute("${LLVMDIS} ${outdir}${basename}.${suffix}.trans.opt.bc -o ${outdir}${basename}.${suffix}.trans.opt.ll");
+
+  print("\nRunning checks on pass \n");
+  if (compare("${outdir}${basename}.${suffix}.pass.log","${outdir}${basename}.${suffix}.pass.log.gold") == 0) {
+    print("\t${basename}: Transformations Passed\n");
+    execute("rm -rf ${outdir}${basename}.${suffix}.pass.log");
+  } else {
+    print("\t${basename}: Transformations Failed\n");
+  }
+
+  generate_linked_binary("${outdir}${basename}.${suffix}.trans.bc", "${outdir}${basename}.${suffix}.trans.lifted.exe" );
+
+  ## Check Output
+  execute("echo ${stdin_args} | ./${outdir}${basename}.${suffix}.trans.lifted.exe ${cmd_args} 1>${outdir}after.trans.out 2>&1");
+  execute("echo ${stdin_args} | ./${outdir}${basename}.${suffix}.lifted.exe ${cmd_args} 1>${outdir}before.trans.out 2>&1");
+
+  if(0 == compare("${outdir}before.trans.out", "${outdir}after.trans.out")) {
+    print("\t${basename}: Output Passed\n");
+    execute("rm -rf ./${outdir}${basename}.${suffix}.trans.lifted.exe");
+    execute("rm -rf ${outdir}${basename}.${suffix}.trans.opt.bc");
+    execute("rm -rf ${outdir}after.trans.out");
+  } else {
+    print("\t${basename}: Output Failed\n");
+  }
+
+  ## Generate allexe and check output
+  #execute("rm -rf 	./${outdir}*.allexe");
+  #${LLVMAS} ${outdir}${PROG_PREFIX}.${ext}.trans.ll -o ${outdir}${PROG_PREFIX}.${ext}.trans.bc
+  #${CC} -m64 -I${INCLUDE_DIR} -emit-llvm -c driver_64.c -o ${outdir}driver_64.bc
+  #${BC2ALLVM} ${outdir}driver_64.bc ${outdir}${PROG_PREFIX}.${ext}.trans.bc -o ${outdir}${PROG_PREFIX}.${ext}.trans.allexe
+
+  #@-echo ${STDIN_ARGS} |  ./${outdir}${PROG_PREFIX}.${ext}.trans.allexe ${CMD_ARGS} > ${outdir}after.trans.out	
+  #@-echo ${STDIN_ARGS} |  ./${outdir}${PROG_PREFIX}.${ext}.lifted.exe ${CMD_ARGS} > ${outdir}before.trans.out	
+
+  #@if diff ${outdir}before.trans.out  ${outdir}after.trans.out  > /dev/null; then \
+  #echo "  ${PROG_PREFIX}: ALLEY Output Passed"; \
+  #	rm -rf  ./${outdir}*.exe   ${outdir}after.trans.out ${outdir}*.allexe ; \
+  #else \
+  #		echo "  ${PROG_PREFIX}: ALLEY Output Failed"; \
+  #	fi 
+}
+
 
 sub cleanup {
   print("Cleanup\n");
