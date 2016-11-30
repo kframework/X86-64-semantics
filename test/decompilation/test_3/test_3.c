d9f6c342d204a460f292672cc2014af2c99b471c
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-11-30 02:36:59 -0600

    migration to new reg assign

--- test/decompilation/test_3/test_3.c
+++ test/decompilation/test_3/test_3.c
@@ -1,5 +1,6 @@
 
 
+
 int foo(int* arr, int n) { 
   unsigned int sum = 0 ;
 
@@ -7,4 +8,11 @@ int foo(int* arr, int n) {
     sum += arr[i];
   }
   return sum;
+}
+
+int main() {
+  unsigned int arr[] = {1,2,3,4,5,6,7,8,9,10};
+  int ret = foo(arr, 10);
+  printf("%d",ret );
+
 }
