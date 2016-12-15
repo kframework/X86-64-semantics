0110f37ec5b8fe385381cc436c4d85149b76f7ea
Sandeep Dasgupta <sdasgup3@illinois.edu>
2016-12-14 22:54:30 -0600

    modified test case test_2_1 as the minimal test case to debug parent stack access

--- test/decompilation/test_2_1/test_2_1.c
+++ test/decompilation/test_2_1/test_2_1.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 
 int foo(int a, int b, int c, int d, int e, int f, int size1,  int size2) { 
-  int sum = size1 + size2 ;
-
-  return sum;
+  printf("%d\n", size1 + size2);
+  return 0;
 }
 
 int main(int argc, char** argv) {
- // int arr1[] = {11,12,13,14,15,16,17,18,19,20};
- // int arr2[] = {1,2,3,4,5,6,7,8,9,10};
   return foo(100, 200, 300, 400, 500, 600, 10,20);
 }
