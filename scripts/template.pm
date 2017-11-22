f4eb6d1ceeebfe1378a43ce5b63d20a80ece6aa4
Sandeep Dasgupta <sdasgup3@illinois.edu>
2017-11-22 00:38:39 -0600

    generated additional rules using templates

--- scripts/template.pm
+++ scripts/template.pm
@@ -8,22 +8,46 @@ $VERSION = 1.00;
 @ISA     = qw(Exporter);
 @EXPORT  = qw(generate );
 
-our $SUB = "";
+#our SUBST = "";
 our @TMPLS = (); 
 
 
 my $T = <<"T1";
-  rule <k> 
-    plugin8(MId:MInt, %$SUB) =>
-      updateReg(orMInt(zeroExtend8to64(MId), andMInt(MIs, mi(64,-256)))   , %$SUB) ...</k>
-  <$SUB> MIs </$SUB>
+  rule <k> %SUBST => MI ...</k> <SUBST> MI </SUBST>
 T1
 push @TMPLS, $T;
 
 $T = <<"T1";
-  rule <k> updateReg(MI:MInt, %${SUB}) => . ...</k> <${SUB}> _ => MI </${SUB}>
+  rule <k> updateReg(MI:MInt, %SUBST) => . ...</k> <SUBST> _ => MI </SUBST>
 T1
 push @TMPLS, $T;
+
+$T = <<"T1";
+  rule <k> extractR8(%SUBST) => truncate64to8(MI) ...</k>
+  <SUBST> MI </SUBST>
+T1
+push @TMPLS, $T;
+
+$T = <<"T1";
+  rule <k> extractR16(%SUBST) => truncate64to16(MI) ...</k>
+  <SUBST> MI </SUBST>
+T1
+push @TMPLS, $T;
+
+$T = <<"T1";
+  rule <k> extractR32(%SUBST) => truncate64to32(MI) ...</k>
+  <SUBST> MI </SUBST>
+T1
+push @TMPLS, $T;
+
+$T = <<"T1";
+  rule <k> 
+    plugin8(MId:MInt, %SUBST) =>
+      updateReg(orMInt(zeroExtend8to64(MId), andMInt(MIs, mi(64,-256)))   , %SUBST) ...</k>
+  <SUBST> MIs </SUBST>
+T1
+push @TMPLS, $T;
+
 
 
 
@@ -33,8 +57,9 @@ sub generate {
 
   for my $TMPL (@TMPLS) {
     for my $arg1 (@args1) {
-      $SUB = $arg1;
-      print $TMPL;
+      my $MODTMPL = $TMPL =~ s/SUBST/$arg1/gr;
+      print $MODTMPL;
     }
+    print "\n\n";
   }
 }
