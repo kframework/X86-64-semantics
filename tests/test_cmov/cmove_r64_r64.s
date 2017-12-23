51dfdfc405509905cd333af32b47af2a4276746b
Sandeep Dasgupta <sdasgup3@illinois.edu>
2017-12-23 11:37:30 -0600

    Make sure that the dump of all the regstate happens whenever a fetch is on top of K cell. That way we dont have to add the dummy opcode opcode at the end of each instrs

--- tests/test_cmov/cmove_r64_r64.s
+++ tests/test_cmov/cmove_r64_r64.s
@@ -2,17 +2,10 @@
 .globl _start
 
 _start:
-    inforegisters
     addq $65535, %rax
-    inforegisters
     addq $1, %rbx
-    inforegisters
     adcw %bx, %ax
-    inforegisters
     cmoveq %rax, %rbx
-    inforegisters
     adcw %bx, %ax
-    inforegisters
     cmoveq %rax, %rbx
-    inforegisters
     nop
