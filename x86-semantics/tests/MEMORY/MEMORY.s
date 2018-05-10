6211f4ada7270d84c9bb511d88fc04fc77cbbbc5
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-05-10 13:49:39 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/59: wip

--- x86-semantics/tests/MEMORY/MEMORY.s
+++ x86-semantics/tests/MEMORY/MEMORY.s
@@ -3,15 +3,12 @@
 
 _start:
   // MEMORY  
-  movq $56, %rsp
   movq $61438, %rax
-  movq %rax, (%rsp)
+  movq %rax, -8(%rsp)
 
-  movq $48, %rsp
   addq $0xabcd234500000000, %rax
-  movq %rax, (%rsp)
+  movq %rax, -16(%rsp)
 
-  movq $52, %rsp
-  movq (%rsp), %rbx
+  movq -12(%rsp), %rbx
   
   nop
