86aaa12882dd11d0434f94f857b4f3b0dd6378dc
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-05-12 17:03:48 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/61: add tests

---
+++ tests/MEMORY4/MEMORY.s
@@ -0,0 +1,26 @@
+.section .text
+.globl _start
+
+_start:
+  // MEMORY  
+  movq $61438, %rax
+  movq %rax, (%rsp)
+
+  addq $0xabcd, %rax
+  movq %rax, -8(%rsp)
+
+  addq $0xdcba, %rax
+  movq %rax, -16(, %rsp)
+
+  addq $0xdcba, %rax
+  movq %rax, -32(, %rsp, 1)
+
+  movq (,%rsp), %rbx
+  movq -8(,%rsp), %rcx
+
+  movq $0, %r10
+  movq -16(%rsp,%r10, 1), %rdx
+
+  movq -32(%r10,%rsp, 1), %r11
+  
+  nop
