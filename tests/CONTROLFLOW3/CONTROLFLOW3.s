13948eafc3095d58998a03549aa8363dce6c9c6a
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-05-16 14:14:07 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/64 & https://github.com/sdasgup3/binary-decompilation/issues/65 wip

---
+++ tests/CONTROLFLOW3/CONTROLFLOW3.s
@@ -0,0 +1,41 @@
+.section .text
+.globl main
+
+main:
+  pushq %rbp
+  movq %rsp, %rbp
+  movl $2, -8(%rbp)
+  movl $3, -12(%rbp)
+  movl $10, -4(%rbp)
+  cmpl $0, -4(%rbp)
+  jne L2
+  movl -8(%rbp), %eax
+  cmpl -12(%rbp), %eax
+  jge L3
+  movl -12(%rbp), %eax
+  subl -8(%rbp), %eax
+  movl %eax, -4(%rbp)
+  jmp L5
+L3:
+  movl -8(%rbp), %edx
+  movl -12(%rbp), %eax
+  addl %edx, %eax
+  movl %eax, -4(%rbp)
+  jmp L5
+L2:
+  movl -8(%rbp), %eax
+  cmpl -12(%rbp), %eax
+  jg L6
+  movl -12(%rbp), %eax
+  subl -8(%rbp), %eax
+  addl %eax, -4(%rbp)
+  jmp L5
+L6:
+  movl -8(%rbp), %edx
+  movl -12(%rbp), %eax
+  addl %edx, %eax
+  addl %eax, -4(%rbp)
+L5:
+  movl -4(%rbp), %eax
+  popq %rbp
+  ret
