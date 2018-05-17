3780742255d089bc7c9c9a3e92ecb753b13b17b3
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-05-17 01:56:17 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/64 & https://github.com/sdasgup3/binary-decompilation/issues/65 wip

--- tests/Programs/bubblesort/test.c
+++ tests/Programs/bubblesort/test.c
@@ -1,4 +1,3 @@
-
 void swap(int *xp, int *yp)
 {
     int temp = *xp;
@@ -10,8 +9,6 @@ void bubbleSort(int arr[], int n)
 {
    int i, j;
    for (i = 0; i < n-1; i++)      
- 
-       // Last i elements are already in place   
        for (j = 0; j < n-i-1; j++) 
            if (arr[j] > arr[j+1]) 
               swap(&arr[j], &arr[j+1]);
@@ -20,7 +17,8 @@ void bubbleSort(int arr[], int n)
 
 int main()
 {
-    int arr[] = {0, 1, 2, 3, 4};
+    int arr[] = {4,3, 2, 1 ,0};
     bubbleSort(arr, 5);
     return 0;
 }
+
