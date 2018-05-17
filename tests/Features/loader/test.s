90fae6cdd3d71e6a026d1ddfd932a831e16647c6
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-05-17 03:05:27 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/64 & https://github.com/sdasgup3/binary-decompilation/issues/65: done

--- tests/Features/loader/test.s
+++ tests/Features/loader/test.s
@@ -1,23 +1,9 @@
 main:
   pushq %rbp
   movq %rsp, %rbp
-  //subq $16, %rsp
-  //movl %edi, -4(%rbp)
-  //movq %rsi, -16(%rbp)
-  //movl $3, %esi
-  //movl $2, %edi
-  // call testFunction(int, int)
   nop
-  // leave
-  // ret
 
 testFunction:
   pushq %rbp
   movq %rsp, %rbp
-  //movl %edi, -4(%rbp)
-  //movl %esi, -8(%rbp)
-  //movl -4(%rbp), %edx
-  //movl -8(%rbp), %eax
-  //addl %edx, %eax
   popq %rbp
-  //ret
