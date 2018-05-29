7a8c88d9ca1984e8b5a7ad4b793ae438b2220ce0
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-05-29 11:43:00 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/70: wip

--- x86-semantics/scripts/bvf2K.pl
+++ x86-semantics/scripts/bvf2K.pl
@@ -27,18 +27,49 @@ my $type       = "";
 my $help       = "";
 my $debugprint = 0;
 
-my %opcodeSkipList = (        # Reason of manual generation
-    "pdepq_r64_r64_r64" => 1, # impractically huge strata formula.
+my %scheduleInstructionsList = (
+    "xchgb_r8_r8"      => 1,
+    "xchgb_r8_rh"      => 1,
+    "xchgb_rh_r8"      => 1,
+    "xchgb_rh_rh"      => 1,
+    "xaddb_r8_r8"      => 1,
+    "xaddb_r8_rh"      => 1,
+    "xaddb_rh_r8"      => 1,
+    "xaddb_rh_rh"      => 1,
+    "xaddl_r32_r32"    => 1,
+    "xaddq_r64_r64"    => 1,
+    "xaddw_r16_r16"    => 1,
+    "cmpxchgb_r8_r8"   => 1,
+    "cmpxchgb_r8_rh"   => 1,
+    "cmpxchgb_rh_r8"   => 1,
+    "cmpxchgb_rh_rh"   => 1,
+    "cmpxchgl_r32_r32" => 1,
+    "cmpxchgq_r64_r64" => 1,
+    "cmpxchgw_r16_r16" => 1,
+);
+
+my %opcodeSkipList = (    # Reason of manual generation
+    "pdepq_r64_r64_r64" => 1,    # impractically huge strata formula.
     "pdepl_r32_r32_r32" => 1,
-    "pextl_r32_r32_r32" => 1, # Difficlt to write the semantics in handlers
+    "pextl_r32_r32_r32" => 1,    # Difficlt to write the semantics in handlers
     "pextq_r64_r64_r64" => 1,
-    "cmpxchgb_r8_rh"    => 1, # Schedule instructions
-    "movmskpd_r64_xmm"  => 1, # Huge strata formula.
+
+    "movmskpd_r64_xmm"  => 1,    # Huge strata formula.
     "vmovmskpd_r64_xmm" => 1,
     "vmovmskpd_r64_ymm" => 1,
     "vmovmskpd_r32_xmm" => 1,
     "vmovmskpd_r32_ymm" => 1,
     "movmskpd_r32_xmm"  => 1,
+    "xchgl_eax_r32"     => 1,    # Schedule instruction;
+    "xchgl_r32_eax"     => 1,
+    "xchgl_r32_r32"     => 1,
+    "xchgq_r64_r64"     => 1,
+    "xchgq_r64_rax"     => 1,
+    "xchgq_rax_r64"     => 1,
+    "xchgw_ax_r16"      => 1,
+    "xchgw_r16_ax"      => 1,
+    "xchgw_r16_r16"     => 1,
+
 );
 
 GetOptions(
@@ -61,9 +92,20 @@ if ( exists $opcodeSkipList{$opcode} ) {
     exit(0);
 }
 
-# Infile
-open( my $fp, "<", $kfile ) or die "Can't open $kfile: $!";
-my @lines = <$fp>;
+my $is_schedule = 0;
+
+#if ( exists $scheduleInstructionsList{$opcode} ) {
+#    utils::info("$opcode: Schedule instruction");
+#    $is_schedule = 1;
+#}
+
+my $mainSemantics = getSemantics( $opcode, $kfile, 0 );
+
+my $auxSemantics = "";
+
+#if ($is_schedule) {
+#    $auxSemantics = getSemantics( $opcode, $kfile . ".samereg", 1 );
+#}
 
 #Outfile
 my $outFile =
@@ -78,43 +120,74 @@ if ( "memory" eq $type ) {
     $outFile = $outFile . "memoryInstructions/$opcode.k";
 }
 
-my $instrfolder = kutils::getInstrsFolder(
-    "$utils::home/Github/strata-data/output-strata/instruction-summary",
-    $type, $opcode );
-
-# Read the Target instr
-my $countertargetinstr = "";
-for my $line (@lines) {
-    chomp $line;
-    if ( $line =~ m/code:(.*)/g ) {
-        $countertargetinstr = utils::trim($1);
-    }
-}
-
-my $targetinstr = kutils::getTargetInstr( $opcode, $debugprint, $instrfolder );
-
-if ( $countertargetinstr ne $targetinstr ) {
-    print "CounterInstr: "
-      . $countertargetinstr . "\n"
-      . "TargetInstr: "
-      . $targetinstr . "\n";
-    failInfo("CheckPoint 1");
-}
-
-# Get the operands and association with dummy registers R1, R2, ...
-my $operandListFromOpcode_ref =
-  getOperandListFromOpcode( $opcode, $debugprint );
-my $operandListFromInstr_ref =
-  getOperandListFromInstr( $targetinstr, $debugprint );
-
-#printArray($operandListFromOpcode_ref);
-#printArray($operandListFromInstr_ref);
-
-my $actual2psedoRegs_ref = getDummyRegsForOperands( $operandListFromOpcode_ref,
-    $operandListFromInstr_ref );
-my %actual2psedoRegs = %{$actual2psedoRegs_ref};
-utils::printMap( $actual2psedoRegs_ref, "ActualToPseduRegs", $debugprint );
-
 print "$opcode:\tK Rule at $outFile\n";
-writeKDefn( sanitizeBVF( $opcode, \@lines, $actual2psedoRegs_ref, $debugprint ),
-    $outFile, $opcode, 1, 0, 0 );
+
+writeKDefn( $mainSemantics,
+    $outFile, $opcode, 1, 0, 0, $is_schedule, $auxSemantics );
+
+############################## UTILS ####################################
+
+sub getSemantics {
+    my $opcode      = shift @_;
+    my $kfile       = shift @_;
+    my $is_schedule = shift @_;
+
+    # Infile
+    open( my $fp, "<", $kfile ) or die "Can't open $kfile: $!";
+    my @lines = <$fp>;
+    close $fp;
+
+    my $targetinstr = "";
+    my $instrfolder = kutils::getInstrsFolder(
+        "$utils::home/Github/strata-data/output-strata/instruction-summary",
+        $type, $opcode );
+
+    if ( !$is_schedule ) {
+
+        # Read the Target instr
+        my $countertargetinstr = "";
+        for my $line (@lines) {
+            chomp $line;
+            if ( $line =~ m/code:(.*)/g ) {
+                $countertargetinstr = utils::trim($1);
+            }
+        }
+
+        $targetinstr =
+          kutils::getTargetInstr( $opcode, $debugprint, $instrfolder );
+
+        if ( $countertargetinstr ne $targetinstr ) {
+            print "CounterInstr: "
+              . $countertargetinstr . "\n"
+              . "TargetInstr: "
+              . $targetinstr . "\n";
+            failInfo("CheckPoint 1");
+        }
+    }
+    else {
+        $targetinstr = kutils::getTargetInstr( $opcode . ".samereg",
+            $debugprint, $instrfolder );
+    }
+
+    print "Target: $targetinstr\n";
+
+    # Get the operands and association with dummy registers R1, R2, ...
+    my $operandListFromOpcode_ref =
+      getOperandListFromOpcode( $opcode, $debugprint );
+    my $operandListFromInstr_ref =
+      getOperandListFromInstr( $targetinstr, $debugprint );
+
+    my $actual2psedoRegs_ref =
+      getDummyRegsForOperands( $operandListFromOpcode_ref,
+        $operandListFromInstr_ref );
+    my %actual2psedoRegs = %{$actual2psedoRegs_ref};
+    utils::printMap( $actual2psedoRegs_ref, "ActualToPseduRegs", $debugprint );
+
+    #printArray($operandListFromOpcode_ref);
+    #printArray($operandListFromInstr_ref);
+    my $semantics =
+      sanitizeBVF( $opcode, \@lines, $actual2psedoRegs_ref, $debugprint );
+
+    #print $semantics. "\n";
+    return $semantics;
+}
