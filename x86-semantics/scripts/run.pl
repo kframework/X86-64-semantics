e737b513b5e0542b70d9838b9f660e545adeaa31
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-06-07 13:05:30 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/77: wip

--- x86-semantics/scripts/run.pl
+++ x86-semantics/scripts/run.pl
@@ -21,6 +21,7 @@ my $xrun       = "";
 my $compare    = "";
 my $output     = "";
 my $linker     = "";
+my $testinput  = "";
 my @args       = ();
 my $kstateskip = 0;
 my $kdefn =
@@ -39,6 +40,7 @@ GetOptions(
     "compare"      => \$compare,
     "clean"        => \$clean,
     "outdir:s"     => \$outdir,
+    "testinput:s"  => \$testinput,
     "kstateskip:s" => \$kstateskip,
     "linker:s"     => \$linker,
 ) or die("Error in command line arguments\n");
@@ -65,15 +67,73 @@ sub createEnv {
     return $cargs;
 }
 
+sub createRegArgs {
+
+    my $retVal =
+"'-cREGV= regvalpair(\"RIP\", mi(64, 0)) regvalpair(\"RAX\",  mi(64, 0)) regvalpair(\"RBX\"    , mi(64, 0)) regvalpair(\"RCX\"    , mi(64, 0)) regvalpair(\"RDX\"    , mi(64, 0)) regvalpair(\"RSI\"    , mi(64, 0)) regvalpair(\"RDI\"    , mi(64, 0)) regvalpair(\"RSP\"    , mi(64, 0)) regvalpair(\"RBP\"    , mi(64, 0)) regvalpair(\"R8\"     , mi(64, 0)) regvalpair(\"R9\"     , mi(64, 0)) regvalpair(\"R10\"    , mi(64, 0)) regvalpair(\"R11\"    , mi(64, 0)) regvalpair(\"R12\"    , mi(64, 0)) regvalpair(\"R13\"    , mi(64, 0)) regvalpair(\"R14\"    , mi(64, 0)) regvalpair(\"R15\"    , mi(64, 0)) regvalpair(\"CF\"     , mi(1, 0)) regvalpair(\"PF\" , mi(1, 0)) regvalpair(\"AF\"     , mi(1, 0)) regvalpair(\"ZF\"     , mi(1, 0)) regvalpair(\"SF\"     , mi(1, 0)) regvalpair(\"OF\"     , mi(1, 0)) regvalpair(\"YMM0\"   ,  mi(256, 0)) regvalpair(\"YMM1\"   ,  mi(256, 0)) regvalpair(\"YMM2\"   ,  mi(256, 0)) regvalpair(\"YMM3\"   ,  mi(256, 0)) regvalpair(\"YMM4\"   ,  mi(256, 0)) regvalpair(\"YMM5\"   ,  mi(256, 0)) regvalpair(\"YMM6\"   ,  mi(256, 0)) regvalpair(\"YMM7\"   ,  mi(256, 0)) regvalpair(\"YMM8\"   ,  mi(256, 0)) regvalpair(\"YMM9\"   ,  mi(256, 0)) regvalpair(\"YMM10\"  ,  mi(256, 0)) regvalpair(\"YMM11\"  ,  mi(256, 0)) regvalpair(\"YMM12\"  ,  mi(256, 0)) regvalpair(\"YMM13\"  ,  mi(256, 0)) regvalpair(\"YMM14\"  ,  mi(256, 0)) regvalpair(\"YMM15\"  ,  mi(256, 0))'";
+    if ( "" eq $testinput ) {
+        return $retVal;
+    }
+
+    open( my $fp, "<", $testinput ) or die "cannot open: $!";
+    my @lines = <$fp>;
+    close $fp;
+    $retVal = "'-cREGV= regvalpair(\"RIP\", mi(64, 0))";
+
+    for my $line (@lines) {
+        if ( $line =~ m/^%(\w+)\s+([\dabcdef].*)/ ) {
+            my $reg    = $1;
+            my $val    = utils::trim( $2, " " );
+            my $intval = 0;
+            my $size   = 0;
+
+            if ( $reg =~ m/rf/ ) {
+                next;
+            }
+            if ( $reg =~ m/^ymm/ ) {
+                $size = 256;
+            }
+            elsif ( $reg =~ m/^r/ ) {
+                $size = 64;
+            }
+            elsif ( $reg =~ m/af|cf|of|zf|sf|pf/ ) {
+                $size = 1;
+            }
+            else {
+                next;
+            }
+
+            $intval = utils::toDec( $val, $size );
+            my $reg_uc = uc($reg);
+
+            #print $reg_uc. " "
+            #  . $size
+            #  . "\n\t\t"
+            #  . $val
+            #  . "\n\t\t$intval" . "\n";
+            $retVal = $retVal . " regvalpair(\"$reg_uc\", mi($size, $intval))";
+        }
+    }
+    $retVal = $retVal . "'";
+    return $retVal;
+}
+
+#sub generateOutTC {
+#  my $outFile = shift @_;
+#
+#}
+
 if ( "" ne $krun ) {
     my ( $dir, $basename, $ext ) = utils::split_filename($file);
 
-    $output = "$outdir/$basename.kstate";
+    #$output = "$outdir/$basename.kstate";
 
     my $envArgs = createEnv( \@args );
+    my $regArgs = createRegArgs();
     execute(
-        "time krun -d $kdefn $basename.$ext $envArgs --output-file $output",
-        1 );
+"time krun -d $kdefn $basename.$ext $envArgs $regArgs --output-file $output",
+        1
+    );
 
     my ( $fh_unused, $tmpfile ) = tempfile( "tmpfileXXXXX", DIR => "/tmp/" );
     execute(
@@ -81,8 +141,10 @@ if ( "" ne $krun ) {
         1
     );
 
-    #execute( "", 1 );
-    checkKRunStatus("$outdir/$basename.kstate");
+    #checkKRunStatus("$outdir/$basename.kstate");
+    checkKRunStatus("$output");
+
+    #generateOutTC($output);
 }
 
 if ( "" ne $xrun ) {
