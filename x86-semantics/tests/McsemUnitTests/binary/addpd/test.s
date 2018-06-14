cd40e3c3d3da8d9185a323962ed8b7fcb20b672b
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-06-14 09:35:34 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/77

--- x86-semantics/tests/McsemUnitTests/binary/addpd/test.s
+++ x86-semantics/tests/McsemUnitTests/binary/addpd/test.s
@@ -5,29 +5,125 @@ _start:
   // ADDPDv128m128_04  
   movq  $0,  %rax
   movq  %rax, %xmm4
+  movdqu %xmm4, -16(%rsp)
+  addpd -16(%rsp), %xmm0
+  
+  movq  $0x3f800000,  %rax
+  movq  %rax, %xmm4
+  movdqu %xmm4, -16(%rsp)
+  addpd -16(%rsp), %xmm0
+  
+  movq  $0x40490fdb,  %rax
+  movq  %rax, %xmm4
+  movdqu %xmm4, -16(%rsp)
+  addpd -16(%rsp), %xmm0
+  
+  movq  $0x47c34f80,  %rax
+  movq  %rax, %xmm4
+  movdqu %xmm4, -16(%rsp)
+  addpd -16(%rsp), %xmm0
+  
+  movq  $0x3ff0000000000000,  %rax
+  movq  %rax, %xmm4
+  movdqu %xmm4, -16(%rsp)
+  addpd -16(%rsp), %xmm0
+  
+  movq  $0x400921fb54442d18,  %rax
+  movq  %rax, %xmm4
+  movdqu %xmm4, -16(%rsp)
+  addpd -16(%rsp), %xmm0
+  
+  movq  $0x40f869f000000000,  %rax
+  movq  %rax, %xmm4
+  movdqu %xmm4, -16(%rsp)
   addpd -16(%rsp), %xmm0
   
   movq  $0x4141414141414141,  %rax
   movq  %rax, %xmm4
+  movdqu %xmm4, -16(%rsp)
   addpd -16(%rsp), %xmm0
 
 
   // VADDPDv128m128_45  
   movq  $0,  %rax
   movq  %rax, %xmm5
+  movdqu %xmm4, -16(%rsp)
+  vaddpd -16(%rsp), %xmm4, %xmm1
+  
+  movq  $0x3f800000,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -16(%rsp)
+  vaddpd -16(%rsp), %xmm4, %xmm1
+  
+  movq  $0x40490fdb,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -16(%rsp)
+  vaddpd -16(%rsp), %xmm4, %xmm1
+  
+  movq  $0x47c34f80,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -16(%rsp)
+  vaddpd -16(%rsp), %xmm4, %xmm1
+  
+  movq  $0x3ff0000000000000,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -16(%rsp)
+  vaddpd -16(%rsp), %xmm4, %xmm1
+  
+  movq  $0x400921fb54442d18,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -16(%rsp)
+  vaddpd -16(%rsp), %xmm4, %xmm1
+  
+  movq  $0x40f869f000000000,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -16(%rsp)
   vaddpd -16(%rsp), %xmm4, %xmm1
   
   movq  $0x4141414141414141,  %rax
   movq  %rax, %xmm5
+  movdqu %xmm4, -16(%rsp)
   vaddpd -16(%rsp), %xmm4, %xmm1
 
   // VADDPDv256m256_45  
   movq  $0,  %rax
   movq  %rax, %xmm5
+  movdqu %xmm4, -32(%rsp)
+  vaddpd -32(%rsp), %ymm4, %ymm1
+  
+  movq  $0x3f800000,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -32(%rsp)
+  vaddpd -32(%rsp), %ymm4, %ymm1
+  
+  movq  $0x40490fdb,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -32(%rsp)
+  vaddpd -32(%rsp), %ymm4, %ymm1
+  
+  movq  $0x47c34f80,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -32(%rsp)
+  vaddpd -32(%rsp), %ymm4, %ymm1
+  
+  movq  $0x3ff0000000000000,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -32(%rsp)
+  vaddpd -32(%rsp), %ymm4, %ymm1
+  
+  movq  $0x400921fb54442d18,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -32(%rsp)
+  vaddpd -32(%rsp), %ymm4, %ymm1
+  
+  movq  $0x40f869f000000000,  %rax
+  movq  %rax, %xmm5
+  movdqu %xmm4, -32(%rsp)
   vaddpd -32(%rsp), %ymm4, %ymm1
   
   movq  $0x4141414141414141,  %rax
   movq  %rax, %xmm5
+  movdqu %xmm4, -32(%rsp)
   vaddpd -32(%rsp), %ymm4, %ymm1
 
   nop
