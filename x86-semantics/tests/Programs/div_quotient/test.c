899fdcd54bdd87af385d142924d94d501abf5aeb
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-07-01 12:53:47 -0500

    testing: gcc c torture tests

--- x86-semantics/tests/Programs/div_quotient/test.c
+++ x86-semantics/tests/Programs/div_quotient/test.c
@@ -1,4 +1,3 @@
-__attribute__((used, noinline, noclone)) unsigned long long
 foo (unsigned long long x, unsigned long long y, unsigned long long z)
 {
   unsigned long long a = x / y;
@@ -8,16 +7,9 @@ foo (unsigned long long x, unsigned long long y, unsigned long long z)
   return a + b;
 }
 
-void abort(void) {
-   __asm__ ("movq $-1, %rax\n\t"
-            "jmp %rax\n\t");
-}
-
 int
 main ()
 {
-  if (foo (64, 7, 0) != 10 || foo (28, 3, 2) != 14)
-    abort ();
-  return 0;
+  return foo (64, 7, 0);
 }
 
--- x86-semantics/tests/Programs/div_quotient/test.s
+++ x86-semantics/tests/Programs/div_quotient/test.s
@@ -16,24 +16,13 @@ foo:
 	orq	%rax, -16(%rbp)
 	movq	-40(%rbp), %rax
 	xorq	%rax, -8(%rbp)
-	movq	-16(%rbp), %rdx
+	movq	-16(%rbp), %rax
+	movl	%eax, %edx
 	movq	-8(%rbp), %rax
-	addq	%rdx, %rax
+	addl	%edx, %eax
 	popq	%rbp
 	ret
-abort:
-	pushq	%rbp
-	movq	%rsp, %rbp
-#APP
-# 12 "test.c" 1
-	movq $-1, %rax
-	jmp %rax
-	
-# 0 "" 2
-#NO_APP
-	popq	%rbp
-	ret
-.globl	_start
+.globl _start
 _start:
 	pushq	%rbp
 	movq	%rsp, %rbp
@@ -41,17 +30,6 @@ _start:
 	movl	$7, %esi
 	movl	$64, %edi
 	call	foo
-	cmpq	$10, %rax
-	jne	L5
-	movl	$2, %edx
-	movl	$3, %esi
-	movl	$28, %edi
-	call	foo
-	cmpq	$14, %rax
-	je	L6
-L5:
-	call	abort
-L6:
-	movl	$0, %eax
 	popq	%rbp
 	ret
+
