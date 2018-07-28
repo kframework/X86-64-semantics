6a7ea263cfb5420976b1e94eebe6b4bc7bc28b58
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-07-28 13:19:16 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/76

--- x86-semantics/tests/Instructions/shrdl_r32_r32_imm8/test.s
+++ x86-semantics/tests/Instructions/shrdl_r32_r32_imm8/test.s
@@ -5,238 +5,238 @@ _start:
   // SHRDQ  
   movq $0, %rax
   movq $0, %rbx
-  shrdq $0, %eax, %ebx
+  shrdl $0, %eax, %ebx
   
   movq $0, %rax
   movq $0, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $1, %rax
   movq $1, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $0xFF, %rax
   movq $0xFF, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0xFFFF, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFF, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0, %rbx
-  shrdq $1, %eax, %ebx
+  shrdl $1, %eax, %ebx
   
   movq $0, %rax
   movq $0, %rbx
-  shrdq $15, %eax, %ebx
+  shrdl $15, %eax, %ebx
   
   movq $1, %rax
   movq $1, %rbx
-  shrdq $15, %eax, %ebx
+  shrdl $15, %eax, %ebx
   
   movq $0xFF, %rax
   movq $0xFF, %rbx
-  shrdq $15, %eax, %ebx
+  shrdl $15, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0xFFFF, %rbx
-  shrdq $15, %eax, %ebx
+  shrdl $15, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFF, %rbx
-  shrdq $15, %eax, %ebx
+  shrdl $15, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0, %rbx
-  shrdq $15, %eax, %ebx
+  shrdl $15, %eax, %ebx
   
   movq $0, %rax
   movq $0, %rbx
-  shrdq $16, %eax, %ebx
+  shrdl $16, %eax, %ebx
   
   movq $1, %rax
   movq $1, %rbx
-  shrdq $16, %eax, %ebx
+  shrdl $16, %eax, %ebx
   
   movq $0xFF, %rax
   movq $0xFF, %rbx
-  shrdq $16, %eax, %ebx
+  shrdl $16, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0xFFFF, %rbx
-  shrdq $16, %eax, %ebx
+  shrdl $16, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFF, %rbx
-  shrdq $16, %eax, %ebx
+  shrdl $16, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0, %rbx
-  shrdq $16, %eax, %ebx
+  shrdl $16, %eax, %ebx
   
   movq $0xFF, %rax
   movq $0xFF, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0xFFFF, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFF, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0xFFFFFFFFFFFFFFFF, %rax
   movq $0xFFFFFFFFFFFFFFFF, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFFFFFFFFFFFFFF, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0xFFFFFFFFFFFFFFFF, %rax
   movq $0, %rbx
-  shrdq $31, %eax, %ebx
+  shrdl $31, %eax, %ebx
   
   movq $0xFF, %rax
   movq $0xFF, %rbx
-  shrdq $32, %eax, %ebx
+  shrdl $32, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0xFFFF, %rbx
-  shrdq $32, %eax, %ebx
+  shrdl $32, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFF, %rbx
-  shrdq $32, %eax, %ebx
+  shrdl $32, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0, %rbx
-  shrdq $32, %eax, %ebx
+  shrdl $32, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $32, %eax, %ebx
+  shrdl $32, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $32, %eax, %ebx
+  shrdl $32, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0, %rbx
-  shrdq $32, %eax, %ebx
+  shrdl $32, %eax, %ebx
   
   movq $0xFF, %rax
   movq $0xFF, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0xFFFF, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFF, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0xFFFFFFFFFFFFFFFF, %rax
   movq $0xFFFFFFFFFFFFFFFF, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFFFFFFFFFFFFFF, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0xFFFFFFFFFFFFFFFF, %rax
   movq $0, %rbx
-  shrdq $63, %eax, %ebx
+  shrdl $63, %eax, %ebx
   
   movq $0xFF, %rax
   movq $0xFF, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0xFFFF, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFF, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0xFFFF, %rax
   movq $0, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFFFFFF, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0xFFFFFFFF, %rax
   movq $0, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0xFFFFFFFFFFFFFFFF, %rax
   movq $0xFFFFFFFFFFFFFFFF, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0, %rax
   movq $0xFFFFFFFFFFFFFFFF, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
   
   movq $0xFFFFFFFFFFFFFFFF, %rax
   movq $0, %rbx
-  shrdq $64, %eax, %ebx
+  shrdl $64, %eax, %ebx
 
   nop
