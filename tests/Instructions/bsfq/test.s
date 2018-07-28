1190bfa44a3f5b01dfd0c48451967a89bc576379
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-07-28 01:20:56 -0500

    wip

---
+++ tests/Instructions/bsfq/test.s
@@ -0,0 +1,84 @@
+.section .text
+.globl _start
+
+_start:
+  // BSFQ  
+  movq $0, %rcx
+  bsfq %rcx, %rax
+  
+  movq $1, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x80, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x55, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xAA, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x0F, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xF7, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xFE, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x8000, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x5500, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xAA00, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x0F0F, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xF7F7, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xFEFE, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x80000000, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x55000000, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xAA000000, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x0F0F0F0F, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xF7F7F7F7, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xFEFEFEFE, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x8000000000000000, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x5500000000000000, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xAA00000000000000, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0x0F0F0F0F0F0F0F0F, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xF7F7F7F7F7F7F7F7, %rcx
+  bsfq %rcx, %rax
+  
+  movq $0xFEFEFEFEFEFEFEFE, %rcx
+  bsfq %rcx, %rax
+
+  nop
