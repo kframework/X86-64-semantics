b59cdf5a10caa180cc947729c1fdf59e69d49783
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-07-28 11:29:14 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/76

---
+++ x86-semantics/tests/Instructions/shldw_r16_r16_cl/test.s
@@ -0,0 +1,301 @@
+.section .text
+.globl _start
+
+_start:
+  // SHLDW  
+  movq $0, %rax
+  movq $0, %rbx
+  movq $0, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $1, %rax
+  movq $1, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFF, %rax
+  movq $0xFF, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0xFFFF, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFF, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0, %rbx
+  movq $1, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0, %rbx
+  movq $15, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $1, %rax
+  movq $1, %rbx
+  movq $15, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFF, %rax
+  movq $0xFF, %rbx
+  movq $15, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0xFFFF, %rbx
+  movq $15, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFF, %rbx
+  movq $15, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0, %rbx
+  movq $15, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0, %rbx
+  movq $16, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $1, %rax
+  movq $1, %rbx
+  movq $16, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFF, %rax
+  movq $0xFF, %rbx
+  movq $16, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0xFFFF, %rbx
+  movq $16, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFF, %rbx
+  movq $16, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0, %rbx
+  movq $16, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFF, %rax
+  movq $0xFF, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0xFFFF, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFF, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFFFFFFFFFF, %rax
+  movq $0xFFFFFFFFFFFFFFFF, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFFFFFFFFFFFFFF, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFFFFFFFFFF, %rax
+  movq $0, %rbx
+  movq $31, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFF, %rax
+  movq $0xFF, %rbx
+  movq $32, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0xFFFF, %rbx
+  movq $32, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFF, %rbx
+  movq $32, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0, %rbx
+  movq $32, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $32, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $32, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0, %rbx
+  movq $32, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFF, %rax
+  movq $0xFF, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0xFFFF, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFF, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFFFFFFFFFF, %rax
+  movq $0xFFFFFFFFFFFFFFFF, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFFFFFFFFFFFFFF, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFFFFFFFFFF, %rax
+  movq $0, %rbx
+  movq $63, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFF, %rax
+  movq $0xFF, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0xFFFF, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFF, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFF, %rax
+  movq $0, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFFFFFF, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFF, %rax
+  movq $0, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFFFFFFFFFF, %rax
+  movq $0xFFFFFFFFFFFFFFFF, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0, %rax
+  movq $0xFFFFFFFFFFFFFFFF, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+  
+  movq $0xFFFFFFFFFFFFFFFF, %rax
+  movq $0, %rbx
+  movq $64, %rcx
+  shldw %cl, %ax, %bx
+
+  nop
