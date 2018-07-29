65b1a7a6171af164867fc97237fb07b5d086d72d
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-07-29 03:39:25 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/76

---
+++ x86-semantics/tests/Instructions/scas/test.s
@@ -0,0 +1,170 @@
+.section .text
+.globl _start
+
+_start:
+  // SCASB  
+  leaq -8(%rsp), %rdi
+  movq $0, %rax
+  scasb
+  
+  leaq -8(%rsp), %rdi
+  movq $1, %rax
+  scasb
+  
+  leaq -8(%rsp), %rdi
+  movq $0xAA, %rax
+  scasb
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFF, %rax
+  scasb
+
+
+  // SCASW  
+  leaq -8(%rsp), %rdi
+  movq $0, %rax
+  scasw
+  
+  leaq -8(%rsp), %rdi
+  movq $1, %rax
+  scasw
+  
+  leaq -8(%rsp), %rdi
+  movq $0xAA, %rax
+  scasw
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFF, %rax
+  scasw
+  
+  leaq -8(%rsp), %rdi
+  movq $0x4141, %rax
+  scasw
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFF, %rax
+  scasw
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFF00, %rax
+  scasw
+  
+  leaq -8(%rsp), %rdi
+  movq $0xF00F, %rax
+  scasw
+
+  // SCASL  
+  
+  leaq -8(%rsp), %rdi
+  movq $0, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $1, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0xAA, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFF, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0x4141, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFF, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFF00, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0xF00F, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0x41414141, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFF0000, %rax
+  scasl
+  
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFFFFFF, %rax
+  scasl
+
+  // SCASQ  
+  leaq -8(%rsp), %rdi
+  movq $0, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $1, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xAA, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFF, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0x4141, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFF, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFF00, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xF00F, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0x41414141, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFF0000, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFFFFFF, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0x4141414141414141, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFF0000FFFF0000, %rax
+  scasq
+  
+  leaq -8(%rsp), %rdi
+  movq $0xFFFFFFFFFFFFFFFF, %rax
+  scasq
+
+
+  nop
