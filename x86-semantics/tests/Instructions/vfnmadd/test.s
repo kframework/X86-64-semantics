42b6c72ae888241f30e4a75f17655106506f06c1
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-01 04:36:45 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/76

--- x86-semantics/tests/Instructions/vfnmadd/test.s
+++ x86-semantics/tests/Instructions/vfnmadd/test.s
@@ -3,6 +3,7 @@
 
 _start:
   // XCHG  
+  
   movq  $0,  %rax
   movq  %rax, %xmm0
   
@@ -40,6 +41,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $1,  %rax
   movq  %rax, %xmm0
@@ -79,6 +81,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x41,  %rax
   movq  %rax, %xmm0
   
@@ -116,6 +119,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x51,  %rax
   movq  %rax, %xmm0
@@ -155,6 +159,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x80,  %rax
   movq  %rax, %xmm0
   
@@ -192,6 +197,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x55,  %rax
   movq  %rax, %xmm0
@@ -231,6 +237,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0xAA,  %rax
   movq  %rax, %xmm0
   
@@ -268,6 +275,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x0F,  %rax
   movq  %rax, %xmm0
@@ -307,6 +315,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0xF7,  %rax
   movq  %rax, %xmm0
   
@@ -344,6 +353,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0xFE,  %rax
   movq  %rax, %xmm0
@@ -383,6 +393,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0xEF,  %rax
   movq  %rax, %xmm0
   
@@ -420,6 +431,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x7F,  %rax
   movq  %rax, %xmm0
@@ -459,6 +471,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0xFF,  %rax
   movq  %rax, %xmm0
   
@@ -496,6 +509,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x4141,  %rax
   movq  %rax, %xmm0
@@ -535,6 +549,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x5151,  %rax
   movq  %rax, %xmm0
   
@@ -572,6 +587,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x8000,  %rax
   movq  %rax, %xmm0
@@ -611,6 +627,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x5500,  %rax
   movq  %rax, %xmm0
   
@@ -648,6 +665,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x5555,  %rax
   movq  %rax, %xmm0
@@ -687,6 +705,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0xAA00,  %rax
   movq  %rax, %xmm0
   
@@ -724,6 +743,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0xAAAA,  %rax
   movq  %rax, %xmm0
@@ -763,6 +783,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x0F0F,  %rax
   movq  %rax, %xmm0
   
@@ -800,6 +821,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0xF7F7,  %rax
   movq  %rax, %xmm0
@@ -839,6 +861,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0xFEFE,  %rax
   movq  %rax, %xmm0
   
@@ -876,6 +899,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0xEFEF,  %rax
   movq  %rax, %xmm0
@@ -915,6 +939,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x7FFF,  %rax
   movq  %rax, %xmm0
   
@@ -952,6 +977,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0xFFFF,  %rax
   movq  %rax, %xmm0
@@ -991,6 +1017,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x41414141,  %rax
   movq  %rax, %xmm0
   
@@ -1028,6 +1055,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x51515151,  %rax
   movq  %rax, %xmm0
@@ -1067,6 +1095,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x55000000,  %rax
   movq  %rax, %xmm0
   
@@ -1104,6 +1133,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0x55555555,  %rax
   movq  %rax, %xmm0
@@ -1143,6 +1173,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0xAAFFFFFF,  %rax
   movq  %rax, %xmm0
   
@@ -1180,6 +1211,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0xAAAAAAAA,  %rax
   movq  %rax, %xmm0
@@ -1219,6 +1251,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0x0F0F0F0F,  %rax
   movq  %rax, %xmm0
   
@@ -1256,6 +1289,7 @@ _start:
   vfnmadd231ps   %ymm4, %ymm9, %ymm0
   
   vzeroall
+  
   
   movq  $0xF7F7F7F7,  %rax
   movq  %rax, %xmm0
@@ -1295,6 +1329,7 @@ _start:
   
   vzeroall
   
+  
   movq  $0xFEFEFEFE,  %rax
   movq  %rax, %xmm0
   
@@ -1333,7 +1368,593 @@ _start:
   
   vzeroall
   
+  
   movq  $0xEFEFEFEF,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0x7FFFFFFF,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0xFFFFFFFF,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0x4141414141414141,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0x5115151515115151,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0x8000000000000000,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0x5500000000000000,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0x5555555555555555,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0xAA00000000000000,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0xAAAAAAAAAAAAAAAA,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0x0F0F0F0F0F0F0F0F,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0xF7F7F7F7F7F7F7F7,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0xEFEFEFEFEFEFEFEF,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0xFEFEFEFEFEFEFEFE,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0x7FFFFFFFFFFFFFFF,  %rax
+  movq  %rax, %xmm0
+  
+  movddup %xmm0, %xmm1
+  movsldup %xmm0, %xmm2
+  movshdup %xmm0, %xmm3
+  
+  vbroadcastsd %xmm1, %ymm4
+  vbroadcastsd %xmm2, %ymm5
+  vbroadcastsd %xmm3, %ymm6
+  
+  vbroadcastss %xmm1, %ymm7
+  vbroadcastss %xmm2, %ymm8
+  vbroadcastss %xmm3, %ymm9
+  
+  vfnmadd132pd   %xmm4, %xmm5, %xmm0
+  vfnmadd132ps   %xmm4, %xmm6, %xmm0
+  vfnmadd132sd   %xmm4, %xmm7, %xmm0
+  vfnmadd132ss   %xmm4, %xmm8, %xmm0
+  vfnmadd132pd   %ymm4, %ymm9, %ymm0
+  vfnmadd132ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd213pd   %xmm4, %xmm5, %xmm0
+  vfnmadd213ps   %xmm4, %xmm6, %xmm0
+  vfnmadd213sd   %xmm4, %xmm7, %xmm0
+  vfnmadd213ss   %xmm4, %xmm8, %xmm0
+  vfnmadd213pd   %ymm4, %ymm9, %ymm0
+  vfnmadd213ps   %ymm4, %ymm9, %ymm0
+  
+  vfnmadd231pd   %xmm4, %xmm5, %xmm0
+  vfnmadd231ps   %xmm4, %xmm6, %xmm0
+  vfnmadd231sd   %xmm4, %xmm7, %xmm0
+  vfnmadd231ss   %xmm4, %xmm8, %xmm0
+  vfnmadd231pd   %ymm4, %ymm9, %ymm0
+  vfnmadd231ps   %ymm4, %ymm9, %ymm0
+  
+  vzeroall
+  
+  
+  movq  $0xFFFFFFFFFFFFFFFF,  %rax
   movq  %rax, %xmm0
   
   movddup %xmm0, %xmm1
