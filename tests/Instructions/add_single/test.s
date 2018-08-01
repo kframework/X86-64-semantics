97e347d36897cd6a1aaa849d6372ab6f259e3d7e
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-01 01:41:00 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/76

---
+++ tests/Instructions/add_single/test.s
@@ -0,0 +1,130 @@
+.section .text
+.globl _start
+
+_start:
+  movq $2147483648, %rax
+  movq $2147483648, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+
+  movq $1078530011, %rax
+  movq $1078530011, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $1051372203, %rax
+  movq $1051372203, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $1095106560, %rax
+  movq $1095106560, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $1116225274, %rax
+  movq $1116225274, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $3225419776, %rax
+  movq $3225419776, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $1073741825, %rax
+  movq $1073741825, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $3221225473, %rax
+  movq $3221225473, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $4194304, %rax
+  movq $4194304, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $2151677953, %rax
+  movq $2151677953, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $2139095039, %rax
+  movq $2139095039, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $4286578687, %rax
+  movq $4286578687, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $8388607, %rax
+  movq $8388607, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $2155872255, %rax
+  movq $2155872255, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $8388608, %rax
+  movq $8388608, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $12582912, %rax
+  movq $12582912, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+
+  movq $16777215, %rax
+  movq $16777215, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+  movq $1051372203, %rax
+  movq $3198855856, %rbx
+  movq %rax, %xmm0
+  movq %rbx, %xmm1
+  addps  %xmm0, %xmm1
+
+  nop
