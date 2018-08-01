65fd658e2a50395a43bee9810f8366cf258eb4b4
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-01 14:32:43 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/76

---
+++ x86-semantics/tests/Instructions/cvt/test.s
@@ -0,0 +1,220 @@
+.section .text
+.globl _start
+
+_start:
+  movq $2147483648, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1036831949, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1045220557, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1053609165, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1061997773, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1399379109, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1733542428, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2067830734, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2139095040, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $4286578688, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+nop  
+
+  movq $4286578688, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2139095039, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $8388608, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1078530011, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1051372203, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1095106560, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1116225274, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $3225419776, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1073741825, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $3221225473, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $4194304, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2151677953, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2139095039, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $4286578687, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $8388607, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2155872255, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $8388608, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $12582912, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $16777215, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2139095039, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $4286578687, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $1, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2147483649, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2139095040, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $4286578688, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
+
+  movq $2147483647, %rax
+  movq %rax, %xmm0
+  cvtss2sd  %xmm0, %xmm1
+  cvtsd2ss  %xmm1, %xmm0
+
