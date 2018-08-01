78a1da37c88088ce6304d2bf2d32c6fdd0c1780a
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-01 03:33:57 -0500

    https://github.com/sdasgup3/binary-decompilation/issues/76

---
+++ x86-semantics/tests/Instructions/div_quotient_rem/test.s
@@ -0,0 +1,842 @@
+.section .text
+.globl _start
+
+_start:
+  // DIVax  
+  movq $255, %rax
+  movq $1, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $2, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $3, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $4, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $5, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $6, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $7, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $8, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $9, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $10, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $11, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $12, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $13, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $14, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $15, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $16, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $17, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $18, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $19, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $20, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $21, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $22, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $23, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $24, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $25, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $26, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $27, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $28, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $29, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $30, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $31, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $32, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $33, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $34, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $35, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $36, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $37, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $38, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $39, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $40, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $41, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $42, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $43, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $44, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $45, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $46, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $47, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $48, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $49, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $50, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $51, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $52, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $53, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $54, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $55, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $56, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $57, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $58, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $59, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $60, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $61, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $62, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $63, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $64, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $65, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $66, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $67, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $68, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $69, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $70, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $71, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $72, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $73, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $74, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $75, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $76, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $77, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $78, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $79, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $80, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $81, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $82, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $83, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $84, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $85, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $86, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $87, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $88, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $89, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $90, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $91, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $92, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $93, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $94, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $95, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $96, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $97, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $98, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $99, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $100, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $101, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $102, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $103, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $104, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $105, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $106, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $107, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $108, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $109, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $110, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $111, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $112, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $113, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $114, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $115, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $116, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $117, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $118, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $119, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $120, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $121, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $122, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $123, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $124, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $125, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $126, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $127, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $128, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $129, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $130, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $131, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $132, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $133, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $134, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $135, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $136, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $137, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $138, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $139, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $140, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $141, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $142, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $143, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $144, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $145, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $146, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $147, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $148, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $149, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $150, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $151, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $152, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $153, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $154, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $155, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $156, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $157, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $158, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $159, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $160, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $161, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $162, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $163, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $164, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $165, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $166, %rbx
+  
+  divb %bl
+  
+  movq $255, %rax
+  movq $167, %rbx
+  
+  divb %bl
+
+
+  nop
---
+++ x86-semantics/tests/Instructions/idiv_quotient_rem/test.s
@@ -0,0 +1,842 @@
+.section .text
+.globl _start
+
+_start:
+  // DIVax  
+  movq $254, %rax
+  movq $1, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $2, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $3, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $4, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $5, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $6, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $7, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $8, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $9, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $10, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $11, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $12, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $13, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $14, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $15, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $16, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $17, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $18, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $19, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $20, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $21, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $22, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $23, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $24, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $25, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $26, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $27, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $28, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $29, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $30, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $31, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $32, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $33, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $34, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $35, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $36, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $37, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $38, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $39, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $40, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $41, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $42, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $43, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $44, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $45, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $46, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $47, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $48, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $49, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $50, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $51, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $52, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $53, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $54, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $55, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $56, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $57, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $58, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $59, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $60, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $61, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $62, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $63, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $64, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $65, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $66, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $67, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $68, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $69, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $70, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $71, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $72, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $73, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $74, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $75, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $76, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $77, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $78, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $79, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $80, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $81, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $82, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $83, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $84, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $85, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $86, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $87, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $88, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $89, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $90, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $91, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $92, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $93, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $94, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $95, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $96, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $97, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $98, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $99, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $100, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $101, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $102, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $103, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $104, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $105, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $106, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $107, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $108, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $109, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $110, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $111, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $112, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $113, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $114, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $115, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $116, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $117, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $118, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $119, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $120, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $121, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $122, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $123, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $124, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $125, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $126, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $127, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $128, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $129, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $130, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $131, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $132, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $133, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $134, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $135, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $136, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $137, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $138, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $139, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $140, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $141, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $142, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $143, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $144, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $145, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $146, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $147, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $148, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $149, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $150, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $151, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $152, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $153, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $154, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $155, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $156, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $157, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $158, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $159, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $160, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $161, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $162, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $163, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $164, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $165, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $166, %rbx
+  
+  idivb %bl
+  
+  movq $254, %rax
+  movq $167, %rbx
+  
+  idivb %bl
+
+
+  nop
