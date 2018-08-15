30832d72e9d58caa220efa58e9c0d3aa85e4baa1
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-15 05:55:03 -0500

    wip:https://github.com/sdasgup3/binary-decompilation/issues/76

---
+++ x86-semantics/tests/Instructions/repnz_scas/test.s
@@ -0,0 +1,15 @@
+.section .text
+.globl _start
+
+_start:
+  // SCASB  
+  leaq -8(%rsp), %rdi
+  movq $0x1121314151617181, %rax
+  pushq %rax
+  movq $0x0, %rax
+  movq $8, %rcx
+  repne scasb
+
+
+
+  nop
---
+++ x86-semantics/tests/Instructions/repz_scas/test.s
@@ -0,0 +1,14 @@
+.section .text
+.globl _start
+
+_start:
+  // SCASB  
+  leaq -8(%rsp), %rdi
+  movq $0x1121314151617181, %rax
+  pushq %rax
+  movq $8, %rcx
+  repe scasb
+
+
+
+  nop
