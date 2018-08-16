3795c43750738f859af5ce9f5bbe8c488b7da57e
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-15 20:06:34 -0500

    wip:https://github.com/sdasgup3/binary-decompilation/issues/76

--- x86-semantics/tests/Instructions/vcvtps2ph_xmm_xmm_imm8/test.s
+++ x86-semantics/tests/Instructions/vcvtps2ph_xmm_xmm_imm8/test.s
@@ -185,5 +185,314 @@ _start:
   vcvtps2ph  $2, %xmm0, %xmm1
   vcvtps2ph  $3, %xmm0, %xmm1
 
+  // ADCal  
+  movq $0x0, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $2147483648, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1040187392, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1048576000, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1056964608, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1065353216, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1073741824, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1082130432, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1090519040, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1052770304, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1061158912, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1069547520, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1077936128, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1086324736, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1036831949, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1045220557, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1053609165, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1061997773, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1399379109, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1733542428, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $2067830734, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $2139095040, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $4286578688, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $3221225472, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1103626240, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $2139095039, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $8388608, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1078530011, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1051372203, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1095106560, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1116225274, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1077936128, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $3225419776, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $1073741825, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $3221225473, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $4194304, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $2151677953, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $2139095039, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $4286578687, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $8388607, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $2155872255, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $8388608, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $12582912, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+  
+  movq $16777215, %rax
+  movq %rax, %xmm0
+  vcvtps2ph  $0, %xmm0, %xmm1
+  vcvtps2ph  $1, %xmm0, %xmm1
+  vcvtps2ph  $2, %xmm0, %xmm1
+  vcvtps2ph  $3, %xmm0, %xmm1
+
 
   nop
