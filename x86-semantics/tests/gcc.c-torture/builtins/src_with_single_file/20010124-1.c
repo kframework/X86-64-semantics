9d5b3ddc9a67be985351cd01b0d0f95168434ab6
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-16 11:18:32 -0500

    wip:https://github.com/sdasgup3/binary-decompilation/issues/76

---
+++ x86-semantics/tests/gcc.c-torture/builtins/src_with_single_file/20010124-1.c
@@ -0,0 +1,32 @@
+/* Verify that structure return doesn't invoke memcpy on 
+   overlapping objects.  */
+#include "20010124-1-lib.c"
+#include "lib/main.c"
+
+extern void abort (void);
+
+struct S {
+  char stuff[1024];
+};
+
+union U {
+  struct {
+    int space;
+    struct S s;
+  } a;
+  struct {
+    struct S s;
+    int space;
+  } b;
+};
+
+struct S f(struct S *);
+void g(union U *);
+
+void main_test(void)
+{
+  union U u;
+  u.b.s = f(&u.a.s);
+  u.a.s = f(&u.b.s);
+  g(&u);
+}
