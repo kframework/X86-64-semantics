b3446a6f8ae9ee351af0c10ff3be10d69b0551f1
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-20 15:31:34 -0500

    wip:C-library

---
+++ x86-semantics/tests/Programs/putchar/test.s
@@ -0,0 +1,11 @@
+main:      
+        pushq   %rbp
+        movq    %rsp, %rbp
+        subq    $16, %rsp
+        movl    $0, -4(%rbp)
+        movb    $65, -5(%rbp)
+        movsbl  -5(%rbp), %edi
+        callq   putchar
+        addq    $16, %rsp
+        popq    %rbp
+        retq
