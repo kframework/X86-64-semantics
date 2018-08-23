f56c4a9be4c63d381c621f6e3515e852a9b3fdbb
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-23 11:24:30 -0500

    wip:C-library:Added rodata and printf support code

--- x86-semantics/tests/Programs/printf/test.c
+++ x86-semantics/tests/Programs/printf/test.c
@@ -2,7 +2,12 @@
 
 int main () {
     int a =10;
-    printf("ABC=%d", a);
+    //float f = 1.8; // 1072064102
+    //double d = 2.3; // 4612361558371493478
+    char *str = "Sandeep";
+    //printf("int=%d  char*=%s float=%f double=%f", a,str, f, d);
+    printf("int=%d  char*=%s", a,str);
+
     return 0;
 }
 
