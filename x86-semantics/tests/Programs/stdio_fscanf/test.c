96aa637dea4d223cc591f4e58e54ce6313b26cfc
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-08-25 18:55:20 -0500

    wip:C-library:Added rodata and printf support code

--- x86-semantics/tests/Programs/stdio_fscanf/test.c
+++ x86-semantics/tests/Programs/stdio_fscanf/test.c
@@ -4,7 +4,7 @@
 
 int main () {
    char str1[10], str2[10], str3[10];
-   int year;
+   char year[10];
    FILE * fp;
 
    fp = fopen ("file.txt", "w");
@@ -12,12 +12,12 @@ int main () {
    
    fclose(fp);
    fp = fopen ("file.txt", "r");
-   fscanf(fp, "%s %s %s %d", str1, str2, str3, &year);
+   fscanf(fp, "%s %s %s %s", str1, str2, str3, year);
    
    printf("Read String1 |%s|\n", str1 );
    printf("Read String2 |%s|\n", str2 );
    printf("Read String3 |%s|\n", str3 );
-   printf("Read Integer |%d|\n", year );
+   printf("Read Integer |%s|\n", year );
 
    fclose(fp);
    
