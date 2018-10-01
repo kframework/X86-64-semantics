63a600b95d253d30aa1b965e059d1b6886e54064
Sandeep Dasgupta <sdasgup3@illinois.edu>
2018-09-30 19:29:41 -0700

    update format

--- x86-semantics/tests/Programs/stdio_printf/test.s
+++ x86-semantics/tests/Programs/stdio_printf/test.s
@@ -1,38 +1,39 @@
-	.file	"test.c"
-	.section	.rodata
+    .section	.rodata
 LC2:
-	.string	"Sandeep"
+    .string	"Sandeep"
 LC3:
-	.string	"int=%d  char*=%s float=%f double=%f"
-	.text
-	.globl	main
-main:
-	pushq	%rbp
-	movq	%rsp, %rbp
-	subq	$32, %rsp
-	movl	$10, -24(%rbp)
-	vmovss	$LC0(%rip), %xmm0
-	vmovss	%xmm0, -20(%rbp)
-	vmovsd	$LC1(%rip), %xmm0
-	vmovsd	%xmm0, -16(%rbp)
-	movq	$LC2, -8(%rbp)
-	vcvtss2sd	-20(%rbp), %xmm2, %xmm2
-	vmovq	%xmm2, %rax
-	vmovsd	-16(%rbp), %xmm0
-	movq	-8(%rbp), %rdx
-	movl	-24(%rbp), %ecx
-	vmovapd	%xmm0, %xmm1
-	vmovq	%rax, %xmm0
-	movl	%ecx, %esi
-	movl	$LC3, %edi
-	movl	$2, %eax
-	call	printf
-	movl	$0, %eax
-	leave
-	ret
-	.section	.rodata
+    .string	"int=%d  char*=%s float=%f double=%f"
+    .text
+    .globl	main
+.globl _start
+_start:
+    pushq	%rbp
+    movq	%rsp, %rbp
+    subq	$32, %rsp
+    movl	$10, -24(%rbp)
+    vmovss	LC0(%rip), %xmm0
+    vmovss	%xmm0, -20(%rbp)
+    vmovsd	LC1(%rip), %xmm0
+    vmovsd	%xmm0, -16(%rbp)
+    leaq	LC2(%rip), %rax
+    movq	%rax, -8(%rbp)
+    vcvtss2sd	-20(%rbp), %xmm2, %xmm2
+    vmovq	%xmm2, %rax
+    vmovsd	-16(%rbp), %xmm0
+    movq	-8(%rbp), %rdx
+    movl	-24(%rbp), %ecx
+    vmovapd	%xmm0, %xmm1
+    vmovq	%rax, %xmm0
+    movl	%ecx, %esi
+    leaq	LC3(%rip), %rdi
+    movl	$2, %eax
+    call	printf
+    movl	$0, %eax
+    leave
+    ret
+    .section	.rodata
 LC0:
-	.long	1072064102
+    .long	1072064102
 LC1:
-	.long	1717986918
-	.long	1073899110
+    .long	1717986918
+    .long	1073899110
