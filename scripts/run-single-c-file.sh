a6af0be8498f606283a06c6312b8d964e332340e
andrew_miranti <andrew.h.miranti@gmail.com>
2019-01-24 18:22:15 -0600

    Misc small changes

--- scripts/run-single-c-file.sh
+++ scripts/run-single-c-file.sh
@@ -3,6 +3,6 @@ assembler=$(mktemp)
 gcc $1 -fno-diagnostics-show-caret -fdiagnostics-color=never    -O0  -w  -lm  -march=haswell -fno-asynchronous-unwind-tables -mlong-double-64 -mno-80387 -S -o /dev/stdout |
     ../scripts/remove_directives.pl --file /dev/stdin > $assembler &&
     ../scripts/collect_instructions_semantics.pl --file $assembler > /dev/null 2>&1 &&
-    ../scripts/kompile.pl --backend java &&
+    ../scripts/kompile.pl --backend $2 &&
     krun $assembler
 rm $assembler
