4eaeb6a9b7a331af3637722ffedd60a93f27cf55
andrew_miranti <andrew.h.miranti@gmail.com>
2019-03-12 11:31:40 -0500

    Update to how relative jumps work, now consistent with disassembly format

--- semantics/test.c
+++ semantics/test.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int fib(const int n) {
-    if (n == 0) {
-        return 0;
-    } else if (n == 1) {
-        return 1;
-    } else {
-        int nums[n + 1];
-        nums[0] = 0;
-        nums[1] = 1;
-        for (int i = 2; i <= n; ++i) {
-            nums[i] = nums[i - 1] + nums[i - 2];
-        }
-        return nums[n];
-    }
-}
-
-const int elems[] = {0, 1, 5, 10, 20, 40};
-
-int main() {
-    for (int i = 0; i < sizeof(elems) / sizeof(int); ++i) {
-        printf("%d: %d\n", elems[i], fib(elems[i]));
-    }
+int main () {
+   const char * str = "The quick brown fox jumped over the lazy dog";
+   const int len = strlen(str);
+   const char * space = " ";
+   char buf[len + 1];
+   char * sptr = buf;
+   strncpy(buf, str, len);
+   while (sptr != NULL) {
+       puts(strsep(&sptr, space));
+   }
+   return 0;
 }
